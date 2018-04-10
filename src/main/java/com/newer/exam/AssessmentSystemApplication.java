bce3d880cfdfd30d945370e56e695c7ce5046b40
Jeremy Yang <hwyoung.96@gmail.com>
2018-04-10 19:15:49 +0800

    Initial commit

---
+++ src/main/java/com/newer/exam/AssessmentSystemApplication.java
@@ -0,0 +1,12 @@
+package com.newer.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentSystemApplication.class, args);
+	}
+}
